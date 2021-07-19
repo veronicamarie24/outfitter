36bd90badffaac253c4275b20c03282dce11ebdc
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2021-07-18 19:36:08 -0700

    simplifying import statements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navigation from "./components/Navigation";
+import { Navigation, Home } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container">
+    <div className="row">
+      <div className="col">
+          <h1>Header text</h1>
+      </div>
+    </div>
+    <div className="row">
+        <h2>Explore</h2>
+      <div className="col">
+        1 of 3
+      </div>
+      <div className="col">
+        2 of 3
+      </div>
+      <div className="col">
+        3 of 3
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Home;
