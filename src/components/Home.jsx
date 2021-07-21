cd9b5b2e52296e6df3f6d64b5c2e23f1eef53aac
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2021-07-21 16:26:38 -0700

    moving images to public folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,8 @@ function Home() {
         <div className="card">
           <img src={ IMAGES.henley } class="card-img-top" alt=""/>
           <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text"><a href="">summergurl</a></p>
+            <button type="button" class="btn btn-secondary btn-sm">American Eagle</button>
           </div>
         </div>
       </div>
