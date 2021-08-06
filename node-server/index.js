28cc6b40d41bc6222c506124ee1ef63d71791444
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2021-08-05 20:46:58 -0700

    serving react build with express

--- node-server/index.js
+++ node-server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const path = require('path')
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+app.use(express.static(path.join(__dirname, '..', 'react-client', 'build')));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ react-client/build/img/index.js
@@ -0,0 +1,10 @@
+const IMAGES = [
+    { description: 'henley', src: './img/henley.png' },
+    { description: 'jeans', src: './img/jeans.jpg' },
+    { description: 'one-piece', src: './img/one-piece.png' },
+    { description: 'red-tank', src: './img/red-tank.png' },
+    { description: 'shorts', src: './img/shorts.jpg' },
+    { description: 'sweaters', src: './img/sweaters.jpg' }
+]
+
+export default IMAGES;
